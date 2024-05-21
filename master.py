731b09d2e768e4a74457711462d70dfdf372e42b
puscianm <teranowa1995@gmail.com>
2024-05-21 20:54:11 +0200

    getTrendingMovies() and processRequest()

--- master.py
+++ master.py
@@ -9,6 +9,26 @@ import pandas as pd
 from datetime import datetime
 import warnings
 
+headers = {
+        "accept": "application/json",
+        "Authorization": apiKey.TMDBBEARER
+    }
+
+def processRequest(url, headers, params) -> pd.DataFrame:
+    """
+    Get response and convert it to pandas.DataFrame with error handling.
+    """
+    response = requests.get(url=url, headers=headers, params=params)
+
+    if response.status_code != 200:
+        print(f"Failed with status code: {response.status_code}")
+        print(response.text)
+        exit(0)
+
+    result_df = pd.DataFrame(response.json()['results'])
+    result_df['downloadDatetime'] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    result_df['page'] = response.json()['page']
+    return result_df
 
 def getPopularMovies(language : str, page : int):
     """
@@ -24,36 +44,31 @@ def getPopularMovies(language : str, page : int):
 
     tmdbPopularURL = "https://api.themoviedb.org/3/movie/popular"
 
-    headers = {
-        "accept": "application/json",
-        "Authorization": apiKey.TMDBBEARER
-    }
-
     params = {
         "language": language,
         "page": str(page)
     }
+    return processRequest(tmdbPopularURL, headers, params)
 
-    response = requests.get(tmdbPopularURL, headers=headers, params=params)
+def getTrendingMovies():
+    """
+    Gets trending movies this week from TMDB.
+    """
+    url = "https://api.themoviedb.org/3/trending/movie/week?language=en-US"
 
-    result_df = pd.DataFrame(response.json()['results'])
-    result_df['downloadDatetime'] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    result_df['page'] = response.json()['page']
-    return result_df
+    params = {
+        "time_window" : "week",
+        "language" : "en-US"
+    }
+
+    return processRequest(url, headers=headers, params=params)
 
 def discover():
     """
-    Uses tbdb database for discovering new titles
-
-
+    Uses tbdb database for discovering new movies.
     """
     warnings.warn("This function is not yet implemented. Use getPopularMovies() for now")
     tmdbDiscoverURL = "https://api.themoviedb.org/3/discover/movie"
-
-    headers = {
-        "accept": "application/json",
-        "Authorization": apiKey.TMDBBEARER
-    }
 
     params = {
         "include_adult": "false",
@@ -63,23 +78,14 @@ def discover():
         "sort_by": "vote_average.desc, vote_count.desc"
     }
 
-    response = requests.get(tmdbDiscoverURL, headers=headers, params=params)
+    return processRequest(url=tmdbDiscoverURL, headers=headers, params=params)
 
-    if response.status_code == 200:
-        json_formatted_str = json.dumps(response.json(), indent=3)
-    else:
-        print(f"Failed with status code: {response.status_code}")
-        print(response.text)
-
-    result_df = pd.DataFrame(response.json())
-    result_df['downloadDatetime'] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    result_df['page'] = response.json()['page']
-    return result_df
 
 if __name__ == "__main__":
-    tmdb_database = pd.read_parquet("tmdb_database.parquet")
-    tmdb_latest_page = tmdb_database['page'].max()
-    
-    tmdb_database = pd.concat([tmdb_database, getPopularMovies("en-US", tmdb_latest_page + 1)],
-                              axis="rows")
-    tmdb_database.to_parquet("tmdb_database.parquet")
+    #tmdb_database = pd.read_parquet("tmdb_database.parquet")
+    #tmdb_latest_page = tmdb_database['page'].max()
+    #
+    #tmdb_database = pd.concat([tmdb_database, getPopularMovies("en-US", tmdb_latest_page + 1)],
+    #                          axis="rows")
+    #tmdb_database.to_parquet("tmdb_database.parquet")
+    getTrendingMovies().to_parquet("tmdb_trending.parquet")
